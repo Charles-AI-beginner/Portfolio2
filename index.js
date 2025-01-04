a87419f6828fbb5a4f9c2845af685900e00fc057
Charles-AI-beginner <priyans2783@gmail.com>
2025-01-04 14:50:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var typed=new Typed(".auto-type",{
+    strings: ["Web Developer","Youtuber","Gaming Geek","Video Editor"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
+document.querySelector(".contact").addEventListener("click", function() {
+    alert("priyans2783@gmail.com");
+});
